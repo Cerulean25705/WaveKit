23d3116d61fdb1d4173a1d265778d6bb8b08780d
Brendan Chaplin <brendanchaplin@users.noreply.github.com>
2026-07-02 21:42:28 +1000

    Enable Firebase project config

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,10 +1,11 @@
-export const firebaseEnabled = false;
+export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBxaqvPXqRPOHw9c0THd4uSZB0rNlp4snI",
+  authDomain: "wavekit-d9e9a.firebaseapp.com",
+  projectId: "wavekit-d9e9a",
+  storageBucket: "wavekit-d9e9a.firebasestorage.app",
+  messagingSenderId: "575328384869",
+  appId: "1:575328384869:web:10a4cf1bb6c7a71468d38c",
+  measurementId: "G-KPQCWVRQXQ"
 };
