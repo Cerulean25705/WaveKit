c967a2b8c7a9e21bddc1691d8f4f6c1647390bbb
Brendan Chaplin <brendanchaplin@users.noreply.github.com>
2026-07-17 17:58:54 +1000

    Fix weapon filter alignment

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "wavekit-shell-v22";
+const CACHE_NAME = "wavekit-shell-v23";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/site.webmanifest",
   "/weapons/",
-  "/styles.css?v=my-wavekit-8",
+  "/styles.css?v=my-wavekit-9",
   "/characters/",
   "/assets/characters-page.js?v=characters-directory-1",
   "/app.js?v=my-wavekit-11",
